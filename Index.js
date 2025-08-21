87d445288d0a6e91ebd70c511c2f06e83bab2c9f
Baby-Boy07 <amfp.2706@gmail.com>
2025-08-21 12:20:00 +0530

    push

--- Index.js
+++ Index.js
@@ -4,7 +4,7 @@ const loudness = require('loudness');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
